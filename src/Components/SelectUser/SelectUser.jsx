f639fb91905e100ada1f50493b0775abaec0d64f
Akansh Mittal <akanshmittal9045@gmail.com>
2022-10-16 21:15:56 +0530

    Select User

--- src/Components/SelectUser/SelectUser.jsx
+++ src/Components/SelectUser/SelectUser.jsx
@@ -1,5 +1,9 @@
 import { React } from "react";
 // import { ProfDashboard } from "./Dashboard";
+import { GiTeacher } from "react-icons/gi";
+import { SiGooglescholar } from "react-icons/si";
+import teacher from "../../images/teacher.png";
+import student from "../../images/student.png";
 
 export default function SelectUser() {
   return (
@@ -14,154 +18,59 @@ export default function SelectUser() {
         <div className="absolute bg-gradient-to-b from-green-500 to-green-400 opacity-75 inset-0 z-0"></div>
         <div
           className="min-h-screen sm:flex sm:flex-row mx-0"
-          style={{ justifyContent: "space-evenly" }}
+          style={{ justifyContent: "center" }}
         >
-          <div class="flex justify-center self-center  z-10">
-            <div class="p-12 bg-white mx-auto rounded-2xl w-100 ">
-              <div class="mb-4">
-                <h3 class="font-semibold text-2xl text-gray-800">Sign In </h3>
-                <p class="text-gray-500">Please sign in to your account.</p>
-              </div>
-              <div class="space-y-5">
-                <div class="space-y-2">
-                  <label class="text-sm font-medium text-gray-700 tracking-wide">
-                    Email
-                  </label>
-                  <input
-                    class=" w-full text-base px-4 py-2 border  border-gray-300 rounded-lg focus:outline-none focus:border-green-400"
-                    type=""
-                    placeholder="mail@gmail.com"
-                  />
+          <h1
+            style={{
+              zIndex: "10",
+              position: "absolute",
+              fontSize: "80px",
+              color: "white",
+              top: "4rem",
+              letterSpacing: "1rem",
+            }}
+          >
+            OSLO
+          </h1>
+          <a
+            class="teacher_card flex justify-center self-center  z-10"
+            href="/LoginProf"
+          >
+            <div style={{ marginRight: "5rem", cursor: "pointer" }}>
+              <div class="p-12 bg-white mx-auto rounded-2xl w-100 ">
+                <div class="mb-4">
+                  <h3 class="font-semibold text-2xl text-gray-800">
+                    Are you a professor ?{" "}
+                  </h3>
                 </div>
-                <div class="space-y-2">
-                  <label class="mb-5 text-sm font-medium text-gray-700 tracking-wide">
-                    Password
-                  </label>
-                  <input
-                    class="w-full content-center text-base px-4 py-2 border  border-gray-300 rounded-lg focus:outline-none focus:border-green-400"
-                    type=""
-                    placeholder="Enter your password"
-                  />
-                </div>
-                <div class="flex items-center justify-between">
-                  <div class="flex items-center">
-                    <input
-                      id="remember_me"
-                      name="remember_me"
-                      type="checkbox"
-                      class="h-4 w-4 bg-blue-500 focus:ring-blue-400 border-gray-300 rounded"
-                    />
-                    <label
-                      for="remember_me"
-                      class="ml-2 block text-sm text-gray-800"
-                    >
-                      Remember me
-                    </label>
-                  </div>
-                  <div class="text-sm">
-                    <a href="#" class="text-green-400 hover:text-green-500">
-                      Forgot your password?
-                    </a>
-                  </div>
-                </div>
-                <div>
-                  <button
-                    type="submit"
-                    class="w-full flex justify-center bg-green-400  hover:bg-green-500 text-gray-100 p-3  rounded-full tracking-wide font-semibold  shadow-lg cursor-pointer transition ease-in duration-500"
-                  >
-                    Sign in
-                  </button>
-                </div>
-              </div>
-              <div class="pt-5 text-center text-gray-400 text-xs">
-                <span>
-                  Copyright © 2021-2022
-                  <a
-                    href="https://codepen.io/uidesignhub"
-                    rel=""
-                    target="_blank"
-                    title="Ajimon"
-                    class="text-green hover:text-green-500 "
-                  >
-                    AJI
-                  </a>
-                </span>
+                <img
+                  src={teacher}
+                  className="mx-auto my-auto"
+                  style={{ width: "8rem", height: "8rem" }}
+                />
               </div>
             </div>
-          </div>
-          <div class="flex justify-center self-center  z-10">
-            <div class="p-12 bg-white mx-auto rounded-2xl w-100 ">
-              <div class="mb-4">
-                <h3 class="font-semibold text-2xl text-gray-800">Sign In </h3>
-                <p class="text-gray-500">Please sign in to your account.</p>
-              </div>
-              <div class="space-y-5">
-                <div class="space-y-2">
-                  <label class="text-sm font-medium text-gray-700 tracking-wide">
-                    Email
-                  </label>
-                  <input
-                    class=" w-full text-base px-4 py-2 border  border-gray-300 rounded-lg focus:outline-none focus:border-green-400"
-                    type=""
-                    placeholder="mail@gmail.com"
-                  />
+          </a>
+
+          <a
+            class="student_card flex justify-center self-center  z-10"
+            href="/LoginUser"
+          >
+            <div style={{ marginLeft: "5rem", cursor: "pointer" }}>
+              <div class="p-12 bg-white mx-auto rounded-2xl w-100 ">
+                <div class="mb-4">
+                  <h3 class="font-semibold text-2xl text-gray-800">
+                    Are you a student ?{" "}
+                  </h3>
                 </div>
-                <div class="space-y-2">
-                  <label class="mb-5 text-sm font-medium text-gray-700 tracking-wide">
-                    Password
-                  </label>
-                  <input
-                    class="w-full content-center text-base px-4 py-2 border  border-gray-300 rounded-lg focus:outline-none focus:border-green-400"
-                    type=""
-                    placeholder="Enter your password"
-                  />
-                </div>
-                <div class="flex items-center justify-between">
-                  <div class="flex items-center">
-                    <input
-                      id="remember_me"
-                      name="remember_me"
-                      type="checkbox"
-                      class="h-4 w-4 bg-blue-500 focus:ring-blue-400 border-gray-300 rounded"
-                    />
-                    <label
-                      for="remember_me"
-                      class="ml-2 block text-sm text-gray-800"
-                    >
-                      Remember me
-                    </label>
-                  </div>
-                  <div class="text-sm">
-                    <a href="#" class="text-green-400 hover:text-green-500">
-                      Forgot your password?
-                    </a>
-                  </div>
-                </div>
-                <div>
-                  <button
-                    type="submit"
-                    class="w-full flex justify-center bg-green-400  hover:bg-green-500 text-gray-100 p-3  rounded-full tracking-wide font-semibold  shadow-lg cursor-pointer transition ease-in duration-500"
-                  >
-                    Sign in
-                  </button>
-                </div>
-              </div>
-              <div class="pt-5 text-center text-gray-400 text-xs">
-                <span>
-                  Copyright © 2021-2022
-                  <a
-                    href="https://codepen.io/uidesignhub"
-                    rel=""
-                    target="_blank"
-                    title="Ajimon"
-                    class="text-green hover:text-green-500 "
-                  >
-                    AJI
-                  </a>
-                </span>
+                <img
+                  src={student}
+                  className="mx-auto my-auto"
+                  style={{ width: "8rem", height: "8rem" }}
+                />
               </div>
             </div>
-          </div>
+          </a>
         </div>
       </div>
     </>
