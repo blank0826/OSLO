ec45f6414e7caa01c07ff3a080a6e55e6963e1c4
blank0826 <aditya26052002@gmail.com>
2022-12-01 05:08:10 +0530

    Aditya

--- src/Components/Profile/UserProfile.jsx
+++ src/Components/Profile/UserProfile.jsx
@@ -2,9 +2,9 @@ import { React, useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { HandleSignupUser, getStudentData } from "../../Firebase";
 import logo from "../../images/logo.png";
-import { Uploader } from "uploader";
-import { UploadDropzone } from "react-uploader";
 import CardProfile from "./CardProfile";
+import Graph from "../../Graph";
+import { ActionCodeURL } from "firebase/auth";
 
 export default function UserProfile() {
   const [name, setName] = useState("");
@@ -13,20 +13,47 @@ export default function UserProfile() {
   const [repeat, setRepeat] = useState("");
   const [rollnumber, setRollNumber] = useState("");
   const [studentData, setStudentData] = useState([]);
+  const [chartData, setChartData] = useState([]);
   const navigate = useNavigate();
-
-  const options = { multi: false };
-
-  const uploader = Uploader({
-    apiKey: "free",
-  });
-
+  let chart_data = [];
   useEffect(() => {
     if (studentData.length == 0) {
       console.log(studentData);
       setStudentData(getStudentData());
     } else {
-      console.log(studentData[0]);
+      var arr = Array.from(Array(7).keys()).map((idx) => {
+        const d = new Date();
+        d.setDate(d.getDate() - d.getDay() + idx);
+        return d.getDate() + "/" + (d.getMonth() + 1) + "/" + d.getFullYear();
+      });
+
+      var values = Array.from(Array(7).keys()).map((idx) => {
+        const d = new Date();
+        d.setDate(d.getDate() - d.getDay() + idx);
+        var date =
+          d.getDate() + "_" + (d.getMonth() + 1) + "_" + d.getFullYear();
+
+        var act = studentData[0].activity[date];
+
+        if (act != undefined) {
+          return act.work;
+        } else {
+          return 0;
+        }
+      });
+
+      console.log(values);
+
+      console.log(studentData);
+
+      chart_data = {
+        chartData: {
+          labels: arr,
+          data: values,
+        },
+      };
+
+      setChartData(chart_data);
     }
   }, [studentData]);
 
@@ -59,21 +86,6 @@ export default function UserProfile() {
       <section class="text-gray-600 body-font" style={{ width: "100%" }}>
         {/* <div class="container px-5 py-24 mx-auto"> */}
         <div class="mx-auto flex flex-wrap">
-          {/* <UploadDropzone
-            uploader={uploader} // Required.
-            options={options} // Optional.
-            width="300px" // Optional.
-            height="300px" // Optional.
-            onUpdate={(files) => {
-              // Optional.
-              if (files.length === 0) {
-                console.log("No files selected.");
-              } else {
-                console.log("Files uploaded:");
-                console.log(files.map((f) => console.log(f.fileUrl)));
-              }
-            }}
-          /> */}
           <CardProfile></CardProfile>
           {studentData.length != 0 ? (
             <div class="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
@@ -84,46 +96,7 @@ export default function UserProfile() {
                 {studentData[0].name}
               </h1>
               <div class="flex mb-4"></div>
-              {/* <p class="leading-relaxed">
-                Fam locavore kickstarter distillery. Mixtape chillwave tumeric
-                sriracha taximy chia microdosing tilde DIY. XOXO fam indxgo
-                juiceramps cornhole raw denim forage brooklyn. Everyday carry +1
-                seitan poutine tumeric. Gastropub blue bottle austin listicle
-                pour-over, neutra jean shorts keytar banjo tattooed umami
-                cardigan.
-  </p> */}
-              <div class="flex items-center border-b-2 border-gray-100 mb-5">
-                {/*} <div class="flex">
-                  <span class="mr-3">Color</span>
-                  <button class="border-2 border-gray-300 rounded-full w-6 h-6 focus:outline-none"></button>
-                  <button class="border-2 border-gray-300 ml-1 bg-gray-700 rounded-full w-6 h-6 focus:outline-none"></button>
-                  <button class="border-2 border-gray-300 ml-1 bg-indigo-500 rounded-full w-6 h-6 focus:outline-none"></button>
-                </div>
-                <div class="flex ml-6 items-center">
-                  <span class="mr-3">Size</span>
-                  <div class="relative">
-                    <select class="rounded border appearance-none border-gray-300 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-200 focus:border-indigo-500 text-base pl-3 pr-10">
-                      <option>SM</option>
-                      <option>M</option>
-                      <option>L</option>
-                      <option>XL</option>
-                    </select>
-                    <span class="absolute right-0 top-0 h-full w-10 text-center text-gray-600 pointer-events-none flex items-center justify-center">
-                      <svg
-                        fill="none"
-                        stroke="currentColor"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        class="w-4 h-4"
-                        viewBox="0 0 24 24"
-                      >
-                        <path d="M6 9l6 6 6-6"></path>
-                      </svg>
-                    </span>
-                  </div>
-                </div> */}
-              </div>
+              <div class="flex items-center border-b-2 border-gray-100 mb-5"></div>
               <div class="flex">
                 <span class="title-font font-medium text-2xl text-gray-900 w-25">
                   Roll Number:
@@ -155,6 +128,21 @@ export default function UserProfile() {
                 <span class="title-font font-medium text-2xl text-gray-500">
                   {" " + convertToDate(studentData[0].lastLogin)}
                 </span>
+              </div>
+              <div class="flex">
+                <span class="title-font font-medium text-2xl text-gray-900">
+                  Activity Graph
+                </span>
+                {chartData.length == 0 ? (
+                  <div className="xl:w-1/2 md:w-1 p-4"></div>
+                ) : (
+                  <div className="p-4">
+                    <Graph
+                      info={chartData}
+                      style={{ height: "400px", width: "400px" }}
+                    />
+                  </div>
+                )}
               </div>
             </div>
           ) : (
---
+++ src/Graph.js
@@ -0,0 +1,113 @@
+import { Line } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  LineElement,
+  PointElement,
+  LinearScale,
+  Title,
+  CategoryScale,
+} from "chart.js";
+
+import { MdAutoGraph } from "react-icons/md";
+
+import React from "react";
+import { startAfter } from "firebase/database";
+
+ChartJS.register(LineElement, PointElement, LinearScale, Title, CategoryScale);
+
+const buildData = ({ chartData }) => ({
+  labels: chartData.labels,
+  datasets: [
+    {
+      label: "",
+      borderColor: "#bae755",
+      borderDash: [5, 5],
+      backgroundColor: "#e755ba",
+      pointBackgroundColor: "#55bae7",
+      pointBorderColor: "#55bae7",
+      pointHoverBackgroundColor: "#55bae7",
+      pointHoverBorderColor: "#55bae7",
+      data: chartData.data,
+      fill: "start",
+      tension: 0.4,
+    },
+  ],
+});
+
+const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      display: false,
+    },
+    title: {
+      display: true,
+      text: "Past 7 Days Activity Graph",
+    },
+  },
+  // scales: {
+  //   y: {
+  //     title: {
+  //       display: true,
+  //       text: "Work Done",
+  //       font: {
+  //         family: "Verdana",
+  //       },
+  //     },
+  //   },
+  //   x: {
+  //     title: {
+  //       display: true,
+  //       text: "Date",
+  //     },
+  //   },
+  // },
+};
+
+const numberToFix = (number, fix) => (number || 0).toFixed(fix);
+
+const Graph = ({ info }) => {
+  console.log(info);
+  const data = buildData(info);
+  console.log(data);
+  let maxval = 0;
+  for (var i = 0; i < data["datasets"][0]["data"].length; i++) {
+    if (maxval < data["datasets"][0]["data"][i]) {
+      maxval = data["datasets"][0]["data"][i];
+    }
+  }
+
+  let total = 0;
+  for (var i = 0; i < data["datasets"][0]["data"].length; i++) {
+    total += data["datasets"][0]["data"][i];
+  }
+
+  return (
+    <>
+      <div
+        className="rounded shadow-xl overflow-hidden w-full md:flex"
+        style={{ width: "450px", height: "300px" }}
+      >
+        <div className="flex w-full px-5 pb-4 pt-8 bg-white-500 text-black items-center">
+          <Line type="line" data={data} options={options} />
+        </div>
+        {/* <div className=" border-gray-200 p-6 rounded-lg">
+          <div className="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
+            <MdAutoGraph style={{ width: "1.5rem", height: "1.5rem" }} />
+          </div>
+          <h2 className="text-lg text-gray-900 font-medium title-font mb-2">
+            Donation Analysis
+          </h2>
+          <p className="leading-relaxed text-base">
+            Monthly Highest donation: ₹{maxval}{" "}
+          </p>
+          <p className="leading-relaxed text-base">
+            Total yearly donation: ₹{total}{" "}
+          </p>
+        </div> */}
+      </div>
+    </>
+  );
+};
+
+export default Graph;
